674a9f9e7d419047a7cb108a042c3904467c0a04
aleksandr.galiew@gmail.com <aleksandr.galiew@gmail.com>
2024-08-28 11:52:08 +0300

    Console

--- Tr1/frmMain.cpp
+++ Tr1/frmMain.cpp
@@ -3,18 +3,94 @@
 #include <Windows.h>
 #include <iostream>
 
+
+#include <vector>
+
+
 using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThreadAttribute]
+
+
+void CONSOLE() {
+	Console::SetWindowPosition(0, 0);
+
+	Console::Write(L"\tOMG! Console::Write(L\"\")\n");
+	Console::WriteLine();
+
+	Console::Beep();
+	Console::Beep(1000, 50);
+	
+	Console::WindowHeight = 50;
+	Console::SetWindowSize(100, 55);
+	Console::Title = "C++\\CLI training.";
+}
+
+
+void STL_VECTORS() {
+	using namespace std;
+
+	vector <int> ar1 = { 1, 2, 3, 4, 5 };
+	ar1.push_back(6);
+	ar1.push_back(7);
+
+	for each (int i in ar1) {
+		cout << i << " ";
+	}
+	cout << endl;
+
+	cout << ar1.size() << endl;
+	cout << ar1[4] << endl;
+	cout << endl;
+
+
+	vector <int> ar2({ 1, 2, 3, 4 });
+
+	for each (int i in ar2) {
+		cout << i << " ";
+	}
+	cout << endl;
+
+	ar2[2] = 10;
+
+	for each (int i in ar2) {
+		cout << i << " ";
+	}
+	cout << endl;
+
+	cout << ar2[2] << endl;
+	cout << ar2.at(2) << endl;
+	
+
+	
+
+
+	/*try							  { cout << ar2[4] << endl; }
+	catch (const std::out_of_range& ex) { cout << "Error !!! Out of range" << endl; cerr << ex.what(); }*/
+		
+	//cout << ar2.at(4) << endl;
+
+}
+
+
+
+
+
+
+
 void APPLICATION_START() {
 	setlocale(LC_ALL, "RU_ru");
 	system("color 70");
 	std::cout << "\n APPLICATION START\n\n";
 	std::cout << "\tStatistic:\n\n";
 
-	
-	
+
+
+	CONSOLE();
+	STL_VECTORS();
+
+
 
 	std::cout << "\n\n";
 }
@@ -39,16 +115,17 @@ int main(array<String^>^ args) {
 
 
 
+
 /// TODO:
 // 
 // (+) MVP
-// ( ) GIT Connection
-// ( ) GIT Make "Develop" branch
+// (+) GIT Connection
+// (+) GIT Make "Develop" branch
 // 
+// (+) CONSOLE
 // -------------------
-// ( ) STL:
+// ( ) STL: Vectors
 // -------------------
-// ( ) Task
 // ( ) Task
 //
 //
--- Tr1/frmMain.h
+++ Tr1/frmMain.h
@@ -17,8 +17,8 @@ namespace Tr1 {
 	protected: ~frmMain() { if (components) delete components; }
 	private: System::Windows::Forms::Button^ button1;
 	protected:
-	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Button^ button3;
+
+
 	private: System::ComponentModel::Container^ components;
 #pragma endregion Kernel
 
@@ -26,47 +26,29 @@ namespace Tr1 {
 		   void InitializeComponent(void)
 		   {
 			   this->button1 = (gcnew System::Windows::Forms::Button());
-			   this->button2 = (gcnew System::Windows::Forms::Button());
-			   this->button3 = (gcnew System::Windows::Forms::Button());
 			   this->SuspendLayout();
 			   // 
 			   // button1
 			   // 
 			   this->button1->Location = System::Drawing::Point(12, 12);
 			   this->button1->Name = L"button1";
-			   this->button1->Size = System::Drawing::Size(75, 23);
+			   this->button1->Size = System::Drawing::Size(260, 228);
 			   this->button1->TabIndex = 0;
-			   this->button1->Text = L"button1";
+			   this->button1->Text = L"Понятно";
 			   this->button1->UseVisualStyleBackColor = true;
-			   // 
-			   // button2
-			   // 
-			   this->button2->Location = System::Drawing::Point(12, 41);
-			   this->button2->Name = L"button2";
-			   this->button2->Size = System::Drawing::Size(75, 23);
-			   this->button2->TabIndex = 1;
-			   this->button2->Text = L"button2";
-			   this->button2->UseVisualStyleBackColor = true;
-			   // 
-			   // button3
-			   // 
-			   this->button3->Location = System::Drawing::Point(12, 70);
-			   this->button3->Name = L"button3";
-			   this->button3->Size = System::Drawing::Size(75, 23);
-			   this->button3->TabIndex = 2;
-			   this->button3->Text = L"button3";
-			   this->button3->UseVisualStyleBackColor = true;
+			   this->button1->Click += gcnew System::EventHandler(this, &frmMain::button1_Click);
+			   this->button1->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &frmMain::button1_KeyDown);
 			   // 
 			   // frmMain
 			   // 
 			   this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
 			   this->ClientSize = System::Drawing::Size(284, 261);
-			   this->Controls->Add(this->button3);
-			   this->Controls->Add(this->button2);
 			   this->Controls->Add(this->button1);
 			   this->Name = L"frmMain";
+			   this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			   this->Text = L"frmMain";
 			   this->Load += gcnew System::EventHandler(this, &frmMain::frmMain_Load);
+			   this->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &frmMain::frmMain_KeyDown);
 			   this->ResumeLayout(false);
 
 		   }
@@ -75,12 +57,18 @@ namespace Tr1 {
 #pragma region Voids
 	private:
 		System::Void frmMain_Load(System::Object^ sender, System::EventArgs^ e) {
-			//
+			this->Focus();
+		}
+		System::Void frmMain_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+			if (e->KeyCode == Keys::Escape) Close();
+		}
+		System::Void button1_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+			if (e->KeyCode == Keys::Escape) Close();
+		}
+		System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+			Close();
 		}
 #pragma endregion
-
-
-
 
 	};
 }
